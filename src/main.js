40fb7063133a490deb442e896a5afe44a2c19542
Eris <hi@eris.cafe>
2025-12-13 17:51:12 -0700

    init

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import { router, loadPage } from "./scripts/router.js";
+
+import "./components/eui-avatar.js";
+import "./components/eui-icon.js";
+import "./components/eui-input.js";
+import "./components/eui-loader.js";
+import "./components/eui-progressbar.js";
+import "./components/eui-switch.js";
+import "./components/eui-chip.js";
+import "./components/eui-app-nav.js";
+import "./components/eui-header.js";
+import "./components/eui-checkbox.js";
+import "./components/eui-button.js";
+import "./components/eui-heading.js";
+import "./components/eui-code.js";
+
+router.add("/", () => loadPage("index"));
+
+router.setNotFound(() => loadPage("404"));
+
+const navItems = [
+    { label: "Home", path: "/", icon: "home" },
+];
+
+window.addEventListener("DOMContentLoaded", () => {
+    const nav = document.querySelector("eui-app-nav");
+    if (nav) nav.navItems = navItems;
+
+    router.navigate(window.location.pathname, false);
+});
