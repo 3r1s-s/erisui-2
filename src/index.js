684fa9125291be55b4f285fb698dcd5dcee98e48
Eris <hi@eris.cafe>
2025-12-16 11:20:06 -0700

    bring back index.js that is used

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import '../styles.css';
+
+import './components/eui-avatar.js';
+import './components/eui-icon.js';
+import './components/eui-input.js';
+import './components/eui-loader.js';
+import './components/eui-progressbar.js';
+import './components/eui-switch.js';
+import './components/eui-chip.js';
+import './components/eui-app-nav.js';
+import './components/eui-nav-item.js';
+import './components/eui-header.js';
+import './components/eui-checkbox.js';
+import './components/eui-button.js';
+import './components/eui-heading.js';
+import './components/eui-code.js';
+
+import { router, loadPage } from './scripts/router.js';
+import * as device from './scripts/device.js';
+import * as haptics from './scripts/haptics.js';
+import * as icons from './scripts/icons.js';
+import * as storage from './scripts/storage.js';
+import * as utils from './scripts/utils.js';
+
+export { router, loadPage, device, haptics, icons, storage, utils };
+
+if (typeof window !== 'undefined') {
+    console.log('ErisUI loaded successfully');
+}
