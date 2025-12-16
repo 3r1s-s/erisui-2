3b36f53f15e0abca210a1ec1fdd00e3ba6df8a16
Eris <hi@eris.cafe>
2025-12-16 11:15:11 -0700

    remove index.js thats not used

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ import "./components/eui-switch.js";
 import "./components/eui-chip.js";
 import "./components/eui-app-nav.js";
 import "./components/eui-app-titlebar.js";
-import "./components/eui-header.js";
 import "./components/eui-checkbox.js";
 import "./components/eui-button.js";
 import "./components/eui-heading.js";
